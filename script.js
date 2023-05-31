102043dfa1f2423d01cb6a11f62467b2e973575c
VladVrt <117577628+VladVrt@users.noreply.github.com>
2023-05-31 15:30:24 +0300

    Add DOM interaction

--- script.js
+++ script.js
@@ -1,77 +1,75 @@
-// Function that plays 5 round and returns the game result
-function game() {
-  // Variable to store game score
-  let playerScore = 0;
-  let computerScore = 0;
-  // Function that returns the computer choice
-  function getComputerChoice() {
-    let choise = Math.floor(Math.random() * 3);
-    let computerChoise;
-    switch (choise) {
-      case 0:
-        computerChoise = "Rock";
-        break;
-      case 1:
-        computerChoise = "Paper";
-        break;
-      case 2:
-        computerChoise = "Scissors";
-        break;
-    }
-    return computerChoise;
+// Function that returns the computer choice
+function getComputerChoice() {
+  let choise = Math.floor(Math.random() * 3);
+  let computerChoise;
+  switch (choise) {
+    case 0:
+      computerChoise = "rock";
+      break;
+    case 1:
+      computerChoise = "paper";
+      break;
+    case 2:
+      computerChoise = "scissors";
+      break;
   }
-  // Function that plays one round and returns the winner
-  function playRound(playerSelection) {
-    playerSelection =
-      playerSelection.charAt(0).toUpperCase() +
-      playerSelection.slice(1).toLowerCase();
-    let computerSelection = getComputerChoice();
-    let gameResult;
-
-    if (playerSelection == computerSelection) {
-      gameResult = `Equal! ${playerSelection} and ${computerSelection} are friends.`;
-    } else if (
-      (playerSelection == "Rock" && computerSelection == "Scissors") ||
-      (playerSelection == "Paper" && computerSelection == "Rock") ||
-      (playerSelection == "Scissors" && computerSelection == "Paper")
-    ) {
-      gameResult = `You Won! ${playerSelection} beats ${computerSelection}.`;
-      playerScore += 1;
-    } else {
-      gameResult = `You Lose! ${computerSelection} beats ${playerSelection}.`;
-      computerScore += 1;
-    }
-    console.log(gameResult);
-  }
-  // Plays the game 5 times
-  for (let i = 0; i < 5; i++) {
-    let playerSelection = prompt(
-      "Please choose your fighter from: Rock, Paper, Scissors"
-    );
-    playerSelection =
-      playerSelection.charAt(0).toUpperCase() +
-      playerSelection.slice(1).toLowerCase();
-    if (
-      !(
-        playerSelection == "Rock" ||
-        playerSelection == "Paper" ||
-        playerSelection == "Scissors"
-      )
-    ) {
-      playerSelection = prompt(
-        "Please choose a valid fighter from: Rock, Paper, Scissors"
-      );
-      }
-    
-      playRound(playerSelection);
-    
-  }
-  // Returns the game winner
-  if (playerScore > computerScore) {
-    console.log("%cYou are the winner!", "color: green");
-  } else if (computerScore > playerScore) {
-    console.log("%cUnfortunately, you lose the game!", "color: red");
-  } else {
-    console.log("%cNo one wins, let's play another round!", "color: blue");
-  }
+  return computerChoise;
 }
+// Function that plays one round and returns the winner
+function playRound() {
+  const newParagraph = document.createElement("p");
+  // Gets the id from the button pressed
+  let playerSelection = this.id;
+  // Variable to store the computer choise
+  let computerChoise = getComputerChoice();
+
+  // Game logic one round
+  if (playerSelection == computerChoise) {
+    gameResult = `Equal! ${playerSelection.charAt(0).toUpperCase() +
+      playerSelection.slice(1).toLowerCase()} and ${computerChoise.charAt(0).toUpperCase() +
+        computerChoise.slice(1).toLowerCase()} are friends.`;
+  } else if (
+    (playerSelection == "rock" && computerChoise == "scissors") ||
+    (playerSelection == "paper" && computerChoise == "rock") ||
+    (playerSelection == "scissors" && computerChoise == "paper")
+  ) {
+    gameResult = `You Won! ${playerSelection.charAt(0).toUpperCase() +
+      playerSelection.slice(1).toLowerCase()} beats ${computerChoise.charAt(0).toUpperCase() +
+        computerChoise.slice(1).toLowerCase()}.`;
+        playerScore+=1;
+  } else {
+    gameResult = `You Lose! ${computerChoise.charAt(0).toUpperCase() +
+      computerChoise.slice(1).toLowerCase()} beats ${playerSelection.charAt(0).toUpperCase() +
+      playerSelection.slice(1).toLowerCase()}.`;
+      computerScore+=1;
+  }
+  // Keeps track the number of games played
+ 
+  // Displays gameResult
+  
+  roundNumber+=1;
+  if (roundNumber==5) {
+    if (playerScore==computerScore){
+      newParagraph.innerText="EQUAL" 
+    }
+    else if (playerScore>computerScore){
+      newParagraph.innerText="YOU WON!"
+    }
+    else {
+      newParagraph.innerText="YOU LOSE!"
+    }
+    document.querySelector(".gameRoundResult").appendChild(newParagraph);
+     playerScore=0;
+     computerScore=0;
+     roundNumber=0;
+  } else {
+    newParagraph.innerText = gameResult;
+    document.querySelector(".gameRoundResult").appendChild(newParagraph);  
+  };
+}
+
+let playerScore=0;
+let computerScore=0;
+let roundNumber=0;
+const buttons = Array.from(document.querySelectorAll("button"));
+buttons.forEach((button) => button.addEventListener("click", playRound));
